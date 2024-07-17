29779eb23175b0d3f69b68895225ac6461e6be49
Ivan Brunetto <ivanbrunetto@gmail.com>
2024-07-17 07:17:15 -0300

    add terser-webpack-plugin

--- starter_project/webpack.prod.js
+++ starter_project/webpack.prod.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
     mode: 'production',
@@ -12,6 +13,7 @@ module.exports = merge(common, {
     optimization: {
         minimizer: [
             new CssMinimizerPlugin(),
+            new TerserPlugin(),
         ],
     },
     module: {
