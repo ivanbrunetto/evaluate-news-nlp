d0a538e5b9dff7ba99b6495c137cdd9ddbfa59c2
Ivan Brunetto <ivanbrunetto@gmail.com>
2024-07-17 06:03:11 -0300

    import css files

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -2,7 +2,11 @@
 import { handleSubmit } from './js/formHandler';
 import { checkForName } from './js/nameChecker';
 
-
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/header.scss';
+import './styles/form.scss';
+import './styles/footer.scss';
 
 //alert("I EXIST");
 // console.log("CHANGE!!");
