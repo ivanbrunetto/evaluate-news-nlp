61753de19c81465708fe9216cfaf455f3f808a2e
Ivan Brunetto <ivanbrunetto@gmail.com>
2024-07-17 06:31:28 -0300

    add css-minimizer-webpack-plugin

--- starter_project/webpack.prod.js
+++ starter_project/webpack.prod.js
@@ -2,11 +2,17 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     output: {
         clean: true,
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
     },
     module: {
         rules: [
