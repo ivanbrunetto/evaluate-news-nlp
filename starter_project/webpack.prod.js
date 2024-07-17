4448a67047c5086629885d2cf535d6eda1d445f5
Ivan Brunetto <ivanbrunetto@gmail.com>
2024-07-17 06:26:31 -0300

    add mini-css-extract-plugin

--- starter_project/webpack.prod.js
+++ starter_project/webpack.prod.js
@@ -1,13 +1,23 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     output: {
         clean: true,
     },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
     plugins: [
-        new WorkboxPlugin.GenerateSW()
+        new WorkboxPlugin.GenerateSW(),
+        new MiniCssExtractPlugin({ filename: '[name].css' }),
     ],
 });
